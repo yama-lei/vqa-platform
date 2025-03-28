187ba8eddd21d8ce28ac34c45dfaa0e6418ae023
yama-lei <1908777046@qq.com>
2025-03-28 20:53:12 +0800

    修复静态资源路径问题

--- vite.config.js
+++ vite.config.js
@@ -30,9 +30,20 @@ export default defineConfig({
             return 'vendor';
           }
         },
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js'
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          let extType = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|ico)(\?.*)?$/.test(assetInfo.name)) {
+            extType = 'img'
+          } else if (/\.(woff2?|eot|ttf|otf)(\?.*)?$/.test(assetInfo.name)) {
+            extType = 'fonts'
+          } else if (/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/.test(assetInfo.name)) {
+            extType = 'media'
+          }
+          return `assets/${extType}/[name]-[hash][extname]`
+        },
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js'
       }
     },
     chunkSizeWarningLimit: 2000
